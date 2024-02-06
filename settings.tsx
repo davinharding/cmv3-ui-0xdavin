8a52f60fbbd766a77b7f41bc372797ed2afa5e2e
Davin <davinlharding@gmail.com>
2024-02-06 11:42:56 -0500

    changed names for mints slightly

--- settings.tsx
+++ settings.tsx
@@ -2,9 +2,9 @@
 // label has to be the exact same as in your candy machine config
 export const mintText = [
   { label: "cbarF", mintText: "Free Mint for CBAR Members", buttonLabel: "Mint now!", header: "CBAR Free Mint" },
-  { label: "cbarP", mintText: "Cbar Mint: 0.15 SOL", buttonLabel: "Mint now!", header: "CBAR Paid Mint" },
-  { label: "wlGF", mintText: "Guaranteed Free Mint for WL", buttonLabel: "Mint now!", header: "WL Guaranteed"}, // DAVIN: modify and match to current al in allowlist.tsx
-  { label: "wlGP", mintText: "WL Mint: 0.2 SOL", buttonLabel: "Mint now!", header: "WL Guaranteed"},
+  { label: "cbarP", mintText: "Cbar Mint: 0.15 SOL", buttonLabel: "Mint now!", header: "CBAR SOL Mint" },
+  { label: "wlGF", mintText: "Guaranteed Free Mint for WL", buttonLabel: "Mint now!", header: "WL Guaranteed Free"}, // DAVIN: modify and match to current al in allowlist.tsx
+  { label: "wlGP", mintText: "WL Mint: 0.2 SOL", buttonLabel: "Mint now!", header: "WL Guaranteed SOL"},
   { label: "wlFCFS", mintText: "WL FCFS Mint: 0.2 SOL", buttonLabel: "Mint now!", header: "WL FCFS"},
   { label: "public", mintText: "Public Mint: 0.25 SOL", buttonLabel: "Mint now!", header: "Public"},
 ];
