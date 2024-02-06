3b2d5e25d8616a92bc8941c1d94a4aa8f4753819
Davin <davinlharding@gmail.com>
2024-02-06 11:36:04 -0500

    updated allowlist and settings for full set, reveal, all guards dry run

--- allowlist.tsx
+++ allowlist.tsx
@@ -20,33 +20,19 @@ export const allowLists = new Map<string, Array<string>>([
   [
     "wlGF",
     [
-      "2ZekZevhKVu7fVntaeraRmJK9xrgkDQfch8PF1FgY62E",
-      "FJx3g6nV6t5Tgo9KbGjuVARt6QKwauyDLVrktmjdQRJk",
-      "42uZwJcKDeqGjLgxB2fhsP5VtubpQak5GJXkXsHYW8o8",
+      "4Wq25E4eMqFe4g95qf6P1E6XyHvk2L4UJn32S39mPfc4",
     ],
   ],
   [
     "wlGP",
     [
-      "2ZekZevhKVu7fVntaeraRmJK9xrgkDQfch8PF1FgY62E",
-      "FJx3g6nV6t5Tgo9KbGjuVARt6QKwauyDLVrktmjdQRJk",
-      "42uZwJcKDeqGjLgxB2fhsP5VtubpQak5GJXkXsHYW8o8",
+      "4Wq25E4eMqFe4g95qf6P1E6XyHvk2L4UJn32S39mPfc4",
     ],
   ],
   [
     "wlFCFS",
     [
-      "2ZekZevhKVu7fVntaeraRmJK9xrgkDQfch8PF1FgY62E",
-      "FJx3g6nV6t5Tgo9KbGjuVARt6QKwauyDLVrktmjdQRJk",
-      "42uZwJcKDeqGjLgxB2fhsP5VtubpQak5GJXkXsHYW8o8",
-    ],
-  ],
-  [
-    "default",
-    [
-      "Tes1zkZkXhgTaMFqVgbgvMsVkRJpq4Y6g54SbDBeKVV",
-      "GjwcWFQYzemBtpUoN5fMAP2FZviTtMRWCmrppGuTthJS",
-      "AT8nPwujHAD14cLojTcB1qdBzA1VXnT6LVGuUd6Y73Cy",
+      "Dtf28H8rgDzj26XxPyexpoMa5ccKnKSp5rVRmjPX9RpL",
     ],
   ],
 ]);
--- settings.tsx
+++ settings.tsx
@@ -2,12 +2,15 @@
 // label has to be the exact same as in your candy machine config
 export const mintText = [
   { label: "cbarF", mintText: "Free Mint for CBAR Members", buttonLabel: "Mint now!", header: "CBAR Free Mint" },
-  { label: "cbarP", mintText: "Cbar Mint Paying with Sol", buttonLabel: "Mint now!", header: "CBAR Paid Mint" },
-  { label: "default", mintText: "Mint Price: 0.25 SOL", buttonLabel: "Mint now!", header: "Default"} // DAVIN: modify and match to current al in allowlist.tsx
+  { label: "cbarP", mintText: "Cbar Mint: 0.15 SOL", buttonLabel: "Mint now!", header: "CBAR Paid Mint" },
+  { label: "wlGF", mintText: "Guaranteed Free Mint for WL", buttonLabel: "Mint now!", header: "WL Guaranteed"}, // DAVIN: modify and match to current al in allowlist.tsx
+  { label: "wlGP", mintText: "WL Mint: 0.2 SOL", buttonLabel: "Mint now!", header: "WL Guaranteed"},
+  { label: "wlFCFS", mintText: "WL FCFS Mint: 0.2 SOL", buttonLabel: "Mint now!", header: "WL FCFS"},
+  { label: "public", mintText: "Public Mint: 0.25 SOL", buttonLabel: "Mint now!", header: "Public"},
 ];
 
 //header image in the ui. replace with your own
 export const image = "/Logo's Projects Black.jpg";
 
 //website title
-export const headerText = "Sol NFT PoC - 0xdavin";
+export const headerText = "Foretelling";
